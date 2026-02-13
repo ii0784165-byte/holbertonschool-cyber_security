453868e96b1a3cac1249d40c7038cd9e9d7dcc45
ii0784165-byte <ii0784165@gmail.com>
2026-02-13 06:29:08 -0500

    task 3

--- cybersecurity_basics/0x03_cryptography_basics/3-password_hash.sh
+++ cybersecurity_basics/0x03_cryptography_basics/3-password_hash.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-SALT=$(openssl rand -hex 8)
-echo -n "$1$SALT" | openssl dgst -sha512 > 3_hash.txt
+#!/bin/bash
+echo -n "$1$(openssl rand -hex 8)" | openssl dgst -sha512 | awk '{print $2}' > 3_hash.txt

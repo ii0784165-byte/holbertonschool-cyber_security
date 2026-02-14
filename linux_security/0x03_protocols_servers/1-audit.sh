0272865e5953c28b79d683d95dee38fa7d4a25b5
ii0784165-byte <ii0784165@gmail.com>
2026-02-14 12:10:10 -0500

    task 1

--- linux_security/0x03_protocols_servers/1-audit.sh
+++ linux_security/0x03_protocols_servers/1-audit.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-for s in "KbdInteractiveAuthentication no" "UsePAM yes" "X11Forwarding yes" "PrintMotd no" "AcceptEnv LANG LC_*" "Subsystem sftp /usr/lib/openssh/sftp-server" "PasswordAuthentication yes" "PermitRootLogin yes" "AuthorizedKeysFile .ssh/authorized_keys" "TCPKeepAlive yes"; do grep -qE "^\s*${s}" /etc/ssh/sshd_config || echo "Non-standard $s"; done
+declare -A standard_values

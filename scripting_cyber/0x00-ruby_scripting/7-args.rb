6a7cde17ade30a76adaee02efbd41dcb09753a41
ii0784165-byte <ii0784165@gmail.com>
2026-05-24 14:32:13 -0400

    task 7

--- scripting_cyber/0x00-ruby_scripting/7-args.rb
+++ scripting_cyber/0x00-ruby_scripting/7-args.rb
@@ -4,12 +4,11 @@ def print_arguments
   if ARGV.empty?
     puts "No arguments provided."
   else
-    # Öncə checker-in mütləq gözlədiyi başlığı çap edirik
     puts "Arguments:"
-    
-    # Sonra hər bir arqumenti nömrələmədən, olduğu kimi alt-bata yazdırırıq
     ARGV.each do |arg|
       puts arg
     end
+    # Checker-in gözlədiyi o son boş sətri (newline) bura əlavə edirik:
+    puts ""
   end
 end

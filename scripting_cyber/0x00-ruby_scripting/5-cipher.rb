168b66745e1394a8d5faf24cd7611be2f78d4a98
ii0784165-byte <ii0784165@gmail.com>
2026-05-24 14:26:00 -0400

    task 5

--- scripting_cyber/0x00-ruby_scripting/5-cipher.rb
+++ scripting_cyber/0x00-ruby_scripting/5-cipher.rb
@@ -1,37 +1,29 @@
 #!/bin/bash
 
 class CaesarCipher
-  # Konstruktor: sürüşmə (shift) dəyərini təyin edir
   def initialize(shift)
     @shift = shift
   end
 
-  # Şifrələmə metodu
   def encrypt(message)
     cipher(message, @shift)
   end
 
-  # Şifrəni açma metodu (sürüşmə dəyərinin mənfisini göndəririk)
   def decrypt(message)
     cipher(message, -@shift)
   end
 
-  # Kənardan birbaşa çağırılmasını əngəlləmək üçün aşağıdakı metodları private edirik
   private
 
-  def cipher(message, shift_value)
-    # Hər bir simvolu tək-tək gəzirik və dəyişdiririk
+  def cipher(message, shift)
     message.chars.map do |char|
       if char.match?(/[A-Z]/)
-        # Böyük hərflər üçün (ASCII 65-90)
-        (((char.ord - 65 + shift_value) % 26) + 65).chr
+        (((char.ord - 65 + shift) % 26) + 65).chr
       elsif char.match?(/[a-z]/)
-        # Kiçik hərflər üçün (ASCII 97-122)
-        (((char.ord - 97 + shift_value) % 26) + 97).chr
+        (((char.ord - 97 + shift) % 26) + 97).chr
       else
-        # Hərf olmayan simvollar (boşluq, nida, vergül və s.) olduğu kimi qalır
         char
       end
-    end.join # Massivə çevrilmiş hərfləri yenidən tək bir string formatında birləşdiririk
+    end.join
   end
 end

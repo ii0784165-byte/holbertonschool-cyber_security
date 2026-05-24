650dade52ed105b675f8835e9cf88363d2564787
ii0784165-byte <ii0784165@gmail.com>
2026-05-24 14:40:47 -0400

    task 9

--- scripting_cyber/0x00-ruby_scripting/8-post.rb
+++ scripting_cyber/0x00-ruby_scripting/8-post.rb
@@ -5,25 +5,28 @@ require 'uri'
 require 'json'
 
 def post_request(url, body_params)
-  # 1. URL-i URI obyektinə çeviririk
   uri = URI.parse(url)
 
-  # 2. POST istəyi üçün obyekt yaradırıq
+  # 1. POST obyekti yaradırıq
   request = Net::HTTP::Post.new(uri)
+  
+  # 2. Kritik Hissə: API-ə JSON göndərdiyimizi bildirən Header-ləri daxil edirik
+  request['Content-Type'] = 'application/json'
+  request['Accept'] = 'application/json'
+  
+  # 3. Parametrləri String-ə yox, təmiz JSON stringinə çevirib body-yə qoyuruq
+  # Bu üsul userId: 11 dəyərinin rəqəm olaraq qalmasını təmin edir
+  request.body = body_params.to_json
 
-  # 3. Parametrləri (Heş obyektini) form data şəklində istəyə əlavə edirik
-  request.set_form_data(body_params)
-
-  # 4. HTTP bağlantısını başladırıq və istəyi göndəririk
+  # 4. İstəyi göndəririk
   response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == 'https') do |http|
     http.request(request)
   end
 
-  # 5. Status kodunu və mesajını ekrana çıxarırıq (Məsələn: 201 Created)
+  # 5. Nəticələri çap edirik
   puts "Response status: #{response.code} #{response.message}"
   puts "Response body:"
 
-  # 6. Gələn cavabı parse edib səliqəli (pretty JSON) formatda çap edirik
   begin
     parsed_body = JSON.parse(response.body)
     puts JSON.pretty_generate(parsed_body)
